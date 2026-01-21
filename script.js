41d501c47b90ed622dcc5b776fe6fe8a3f328dd0
zeuzu <yekososa@gmail.com>
2026-01-20 18:27:43 -0800

    solar panels

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 console.log("test");
+
+function add(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        return NaN;
+    }
+
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+        return NaN;
+    }
+
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+  if (isNaN(num1) || isNaN(num2) || num2 == 0) {
+        return NaN;
+    }
+
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+if (isNaN(num1) || isNaN(num2)) {
+        return NaN;
+    }
+
+    return num1 * num2;
+}
+
+const solarPanel = document.querySelector(".solar");
+for(let i = 0; i < 4; i++) {
+    let panel = document.createElement("div");
+    panel.classList.add("solar-panel");
+    solarPanel.appendChild(panel);
+}
