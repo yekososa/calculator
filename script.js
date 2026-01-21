faaa1f44f489eb6e23e48b97ae2e6c1c03d6e74f
zeuzu <yekososa@gmail.com>
2026-01-20 19:07:58 -0800

    initial column of values

--- script.js
+++ script.js
@@ -38,3 +38,37 @@ for(let i = 0; i < 4; i++) {
     panel.classList.add("solar-panel");
     solarPanel.appendChild(panel);
 }
+
+const buttons = [
+    {val: '+/-', type: 'operator' , color: 'red-btn' },
+    {val: '√', type: 'operator' , color: 'red-btn' },
+    {val: '%', type: 'operator' , color: 'red-btn' },
+    {val: 'MRC', type: 'operator' , color: 'red-btn' },
+    {val: 'M-', type: 'operator' , color: 'red-btn' },
+    {val: 'M+', type: 'operator' , color: 'red-btn' },
+    {val: '7', type: 'operator' , color: 'white-btn' },
+    {val: '8', type: 'operator' , color: 'white-btn' },
+    {val: '9', type: 'operator' , color: 'white-btn' },
+    {val: '4', type: 'operator' , color: 'white-btn' },
+    {val: '5', type: 'operator' , color: 'white-btn' },
+    {val: '6', type: 'operator' , color: 'white-btn' },
+    {val: '1', type: 'operator' , color: 'white-btn' },
+    {val: '2', type: 'operator' , color: 'white-btn' },
+    {val: '3', type: 'operator' , color: 'white-btn' },
+    {val: 'ON', type: 'operator' , color: 'red-btn' },
+    {val: '0', type: 'operator' , color: 'white-btn' },
+    {val: '.', type: 'operator' , color: 'white-btn' },
+];
+
+const percentage = 100/3+ '%';
+const buttonContainer = document.querySelector(".btnContainer");
+buttons.forEach(button => {
+    const btn = document.createElement("button");
+    btn.textContent = button.val;
+    btn.classList.add(button.color);
+    btn.style.flex = `0 0 ${percentage}`
+    buttonContainer.appendChild(btn);
+    
+    
+    // set type behavior
+});
